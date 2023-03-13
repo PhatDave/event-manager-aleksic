838a909b5713948c35e7310e56e396a38c1275cf
ivan-aleksic <ivan.aleksic582@gmail.com>
2023-03-13 10:38:25 +0100

    Added error exception handlers for event not exist and event not accepting registrations cases

--- src/main/java/hr/neos/aleksaeventmanager/exception/GlobalExceptionHandler.java
+++ src/main/java/hr/neos/aleksaeventmanager/exception/GlobalExceptionHandler.java
@@ -4,8 +4,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.MethodNotAllowedException;
 
 import java.util.Date;
+import java.util.NoSuchElementException;
+
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -13,6 +16,18 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorDetails> handleIllegalStateException(IllegalStateException ex) {
         ErrorDetails errorDetails = this.prepareErrorDetails(ex);
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = NoSuchElementException.class)
+    public ResponseEntity<ErrorDetails> handleNoSuchElementException(NoSuchElementException ex){
+        ErrorDetails errorDetails = this.prepareErrorDetails(ex);
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = MethodNotAllowedException.class)
+    public ResponseEntity<ErrorDetails> handleMethodNotAllowedException(MethodNotAllowedException ex) {
+        ErrorDetails errorDetails = this.prepareErrorDetails(ex);
+        return new ResponseEntity<>(errorDetails, HttpStatus.METHOD_NOT_ALLOWED);
     }
 
     private ErrorDetails prepareErrorDetails(Exception exception) {
